5dde82db7d2b169d09e22fa5527d04e8babbf734
dannygallo11 <95456444+dannygallo11@users.noreply.github.com>
2022-12-06 15:25:42 -0600

    Keep track of item searched using useState and triggering get fetch request when form is submitted

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -1,30 +1,56 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import './searchbar.css'
 
 
 
-function SearchBar({getAPI}) {
-    const searchRef = useRef()
+function SearchBar() {
 
+// useState created to keep track of the ingredient or recipe being searched.
+    const [item, setItem] = useState('')
+    console.log(item)
+
+
+// used to test if useState item variable is being updated properly
     function onSubmit(e) {
         e.preventDefault()
-        console.log({search: searchRef.current.value});
+        setItem(document.getElementById('searchBar').value)
+        console.log(item);
     }
+
+// header info for api calls to spoonacular API
+    var myHeaders = new Headers();
+    myHeaders.append("apikey", "dWtOcLMwKxX8LxlurntuLm4pLTD2bMwv");
+
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+    }
+
+// get fetch request once the submit button is clicked. 
+    const getAPI = () => {
+        fetch("https://api.apilayer.com/spoonacular/recipes/complexSearch?query="+ item +"", requestOptions)
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+      
+    }
+
 
     return(
         <div className="searchBar">
-            <form onSubmit={onSubmit}>
+            <form onSubmit={onSubmit}> 
                 <label htmlFor="search">Search for recipe</label><br></br>
                 <input 
-                    ref={searchRef}
                     type="text"
                     placeholder="Insert recipe or ingredient"
                     id="searchBar"
                 />
-                <button onClick={getAPI} type="submit">Search</button>
+                <button onClick={getAPI} type="submit">Search</button> 
             </form>
         </div>
     )
-};
+    };
 
 export default SearchBar;
+
+
